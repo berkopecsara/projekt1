0021ba80fafd399e7a5d90442370b1daabd1f9a6
Sara Berkopec <berkopecsara@gmail.com>
2017-02-08 22:04:23 +0100

    Ugani_stevilo

---
+++ Documents/PROGRAMIRANJE-predavanje 7/DN/Ugani_skrito_stevilo.py
@@ -0,0 +1,37 @@
+#-*- coding: utf-8 -*-
+#Ugani skrito število
+vsota = ("5 + 5")
+print(vsota)
+
+resnica = raw_input("Rezultat: ")
+if resnica == "10" :
+    print("Bravo!")
+else:
+    print("Ups! Poskusi še enkrat!")
+
+vsota = ("15 + 60")
+print (vsota)
+
+resnica = raw_input("Rezultat: ")
+if resnica == "75" :
+    print("Bravo!")
+else:
+    print("Ups! Poskusi še enkrat!")
+
+razlika = ("1200 - 200")
+print (razlika)
+
+resnica = raw_input("Rezultat: ")
+if resnica == "1000":
+    print("Bravo!")
+else:
+    print("Ups! Poskusi še enkrat!")
+
+neznanka = ("5 + y = 10")
+print (neznanka)
+
+resnica = raw_input("y =  ")
+if resnica == "5":
+    print("Bravo!")
+else:
+    print("Ups! Poskusi še enkrat!")
